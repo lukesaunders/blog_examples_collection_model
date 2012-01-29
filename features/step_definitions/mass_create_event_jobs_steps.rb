2127f7f67e885b795a7ff3e29d8d461a39e7a1bf
Luke Saunders <luke.saunders@gmail.com>
2012-01-29 22:24:34 +0000

    filled in steps for event jobs feature

--- features/step_definitions/mass_create_event_jobs_steps.rb
+++ features/step_definitions/mass_create_event_jobs_steps.rb
@@ -1,19 +1,31 @@
 Given /^An event$/ do
-  pending # express the regexp above with the code you wish you had
+  @event = Factory(:event)
 end
 
 When /^I go to the create event jobs page$/ do
-  pending # express the regexp above with the code you wish you had
+  visit event_path(@event)
+  click_link "Create event jobs"
 end
 
 When /^Change some details and submit the form$/ do
-  pending # express the regexp above with the code you wish you had
+  # expect a table with a list of new jobs to be created, each with a field for expected time
+  within page.find(:xpath, "//tr[contains(.//td/text(), \"Erect a flag pole\")]") do
+    fill_in "Expected time", :with => "4.00"
+  end
+  within page.find(:xpath, "//tr[contains(.//td/text(), \"Raise the flag\")]") do
+    fill_in "Expected time", :with => "2.00"
+  end
 end
 
 Then /^All those jobs should be created with my changes$/ do
-  pending # express the regexp above with the code you wish you had
+  visit jobs_path
+  page.should have_content "Raise the flag"
+  page.should have_content "2.00"
+  page.should have_content "Erect a flag pole"
+  page.should have_content "4.00"
 end
 
 Then /^The event should be marked that the jobs are finalized$/ do
-  pending # express the regexp above with the code you wish you had
+  visit edit_path(@event)
+  page.should_not have_content "Create event jobs"
 end
