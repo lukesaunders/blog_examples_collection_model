dbff697fcedbf5b793002dd3c9822152e783b6d1
Luke Saunders <luke.saunders@gmail.com>
2012-01-29 21:13:12 +0000

    Rails boilerplate, with RSpec and Cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollectionModel::Application.config.secret_token = '14fa5af1c97dc17ddb519258b609da9c274eb4994fbbe2ee15b1d97519b17cda28e72159663f74a5a7b9c5ee876aaef79a514143b6fcff4e4aaf4fa1af7ee666'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+end
